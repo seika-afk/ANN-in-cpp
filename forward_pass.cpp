95b3643aaa8f99a2cb9c6bbbfa414d8eb9e892ea
Seika <artistnoob6@gmail.com>
2025-09-17 19:13:47 +0530

    Forward Pass COmpleted

--- forward_pass.cpp
+++ forward_pass.cpp
@@ -10,6 +10,8 @@ class ANN{
 
 	public :
 	string ac_fn;
+	vector<float> inputs;
+	AcFn af;
 
 
 //############################### INITIALIZATION METHODS
@@ -18,7 +20,7 @@ class ANN{
 
 //########################### XAVIER : for tanh and sigmoid
 
-	float xavier_init(int n_in,int n_out){
+	float xavier(int n_in,int n_out){
 	
 //seeding for random gen
 static bool seeded = false;         
@@ -37,7 +39,7 @@ return weight;
 //########################## HE : for relu
 
 
-	float he_init(int n_in){
+	float he(int n_in){
 	
 //seeding for random gen
 static bool seeded = false;         
@@ -49,7 +51,7 @@ static bool seeded = false;
     float p_limit=sqrt(6.0f/(n_in));
     float n_limit=-(p_limit);
     float weight=n_limit+ static_cast<float>(rand()) / static_cast<float>(RAND_MAX )* (p_limit - n_limit);
-	
+	//cout<<"Activated";
 return weight;
 	}
 
@@ -61,23 +63,72 @@ return weight;
 	//ANN(int neurons, int h_layers,string ac_fn,weight_initialization_method){
 		//this->ac_fn=ac_fn;
 
-
-	void forward_pass( const vector<float> inputs){
-		int n_out=1;
-		vector<float> weights ={};
-
-		for (int i=0;i<inputs.size();i++){
-			weights.push_back(he_init(inputs.size()));
-		}
-		cout<<"Weights: ";
-		for (auto w :weights)cout<<w<<" ";
-		cout<<endl;
+// We are expecting input like ,single row, need to iterate of whole dataset themself
+	void input_layer( const vector<float> input){
+	this->inputs=input;
 	}
 
 
-//################3
 
-	};
+	void neuron(int input_shape,int output_shape,string weight_init_method,string ac_fn)
+{
+
+	int n_out=output_shape;
+
+		vector<float> weights ={};
+
+		for (int i=0;i<inputs.size();i++){
+
+			if (weight_init_method=="xavier"){
+			weights.push_back(xavier(inputs.size(),n_out));
+			}
+			else{
+	weights.push_back(he(inputs.size()));
+
+
+			}
+
+
+			}
+
+		//checking weights
+		cout<<"Weights: ";
+		for (auto w :weights)cout<<w<<" ";
+		cout<<endl;
+
+		//next steps :
+		//applying z: weighted sum
+		//applying activation fn  
+		float bias=0.0;
+		float z=0.0;
+
+		for (int i =0; i<inputs.size();i++){
+  			z+=inputs[i]* weights[i];
+
+		} 
+		z=z+bias;
+		cout<<"Weighted Sum is : "<<z<<endl;
+		float activation;
+		// Applying activation fn 
+		if (ac_fn =="relu"){
+		activation=af.ReLU(z);}
+		else{
+activation=af.sigmoid(z);
+
+		}
+		cout<<"FInal Activation"<<activation<<endl;
+
+
+
+
+
+}
+
+	
+
+
+// ################### END OF CLASS
+};
 
 int main(){
 
@@ -89,13 +140,18 @@ vector<float> inputs ={1,2,3};
 //ann.forward_pass(inputs);
 
 //he init
-ANN ann1;
-ann1.forward_pass(inputs);
+//ANN ann1;
+//ann1.forward_pass(inputs);
 
+
+
+// demoing Z
+ANN ann;
+ann.input_layer(inputs);
+ann.neuron(3,1,"he","relu");
 
 return 0;
 }
-
 
 
 
