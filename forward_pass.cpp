37a4a3c264a839ba67077f3831666119bf0a57dc
Seika <artistnoob6@gmail.com>
2025-09-17 17:51:40 +0530

    More weight init methods added

--- forward_pass.cpp
+++ forward_pass.cpp
@@ -14,6 +14,10 @@ class ANN{
 
 //############################### INITIALIZATION METHODS
 
+
+
+//########################### XAVIER : for tanh and sigmoid
+
 	float xavier_init(int n_in,int n_out){
 	
 //seeding for random gen
@@ -25,10 +29,34 @@ static bool seeded = false;
 	
     float p_limit=sqrt(6.0f/(n_in+n_out));
     float n_limit=-(p_limit);
-    float weight=n_limit+ static_cast<float>(rand()) / RAND_MAX * (p_limit - n_limit);
+    float weight=n_limit+ static_cast<float>(rand()) / static_cast<float>(RAND_MAX )* (p_limit - n_limit);
 	
 return weight;
 	}
+
+//########################## HE : for relu
+
+
+	float he_init(int n_in){
+	
+//seeding for random gen
+static bool seeded = false;         
+    if (!seeded) {
+        std::srand(std::time(0));   
+        seeded = true;
+    }
+	
+    float p_limit=sqrt(6.0f/(n_in));
+    float n_limit=-(p_limit);
+    float weight=n_limit+ static_cast<float>(rand()) / static_cast<float>(RAND_MAX )* (p_limit - n_limit);
+	
+return weight;
+	}
+
+
+
+
+
 //########################### FORWARD PASS	
 	//ANN(int neurons, int h_layers,string ac_fn,weight_initialization_method){
 		//this->ac_fn=ac_fn;
@@ -39,7 +67,7 @@ return weight;
 		vector<float> weights ={};
 
 		for (int i=0;i<inputs.size();i++){
-			weights.push_back(xavier_init(inputs.size(),n_out));
+			weights.push_back(he_init(inputs.size()));
 		}
 		cout<<"Weights: ";
 		for (auto w :weights)cout<<w<<" ";
@@ -55,8 +83,16 @@ int main(){
 
 vector<float> inputs ={1,2,3};
 
-ANN ann;
-ann.forward_pass(inputs);
+
+//xavier init
+//ANN ann;
+//ann.forward_pass(inputs);
+
+//he init
+ANN ann1;
+ann1.forward_pass(inputs);
+
+
 return 0;
 }
 
